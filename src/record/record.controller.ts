70b56b2c39d82dfcc0930ac2d7ee0df167375cc2
kurovu146 <kurobakaito146@gmail.com>
2025-02-15 01:06:31 +0700

    update record

--- src/record/record.controller.ts
+++ src/record/record.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Query, Req, UseGuards } from "@nestjs/common";
 import { CreateRecordDTO } from "./dto/create-record.dto";
 import { RecordService } from "./record.service";
+import { UserGuard } from "src/auth/user.guard";
 
 
 @Controller('records')
@@ -11,5 +12,17 @@ export class RecordController {
   @Post()
   async create(@Body() data: CreateRecordDTO) {
     return this.recordService.create(data);
+  }
+
+  @UseGuards(UserGuard)
+  @Get()
+  async getAllVideo(@Query('userId') userId: number) {
+    return this.recordService.getAllVideo(+userId);
+  }
+
+  @UseGuards(UserGuard)
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.recordService.delete(+id);
   }
 }
--- src/record/record.service.ts
+++ src/record/record.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateRecordDTO } from './dto/create-record.dto';
+import { AppError } from 'utils/error';
 
 @Injectable()
 export class RecordService {
@@ -13,5 +14,24 @@ export class RecordService {
 
   async create(data: CreateRecordDTO) {
     return this.prisma.record.create({data});
+  }
+
+  async getAllVideo(userId: number) {
+    if (!userId) {
+      throw new AppError('User not found!', HttpStatus.NOT_FOUND);
+    }
+    const records = await this.prisma.record.findMany({where: {userId}});
+
+    return records.map(item => {return item.url});
+  }
+
+  async delete(id: number) {
+    const record = await this.prisma.record.findFirst({where:{id}});
+
+    if (!record) {
+      throw new AppError('Record not found!', HttpStatus.NOT_FOUND);
+    }
+
+    return this.prisma.record.delete({where: {id}});
   }
 }
